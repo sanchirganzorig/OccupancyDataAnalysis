234da3942332581471a4c4fc77b79cd7a208d6bf
sanchirganzorig <sanchirganzorig@users.noreply.github.com>
2022-02-07 17:30:03 +0100

    removed firebase db

--- grupprum_flask/__init__.py
+++ grupprum_flask/__init__.py
@@ -1,10 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import secrets
-import pyrebase
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -17,7 +13,5 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # }
 
 
-cred = credentials.Certificate(secrets["GOOGLE_APPLICATION_CREDENTIALS"])
-firebase_admin.initialize_app(cred, {'databaseURL': secrets["databaseURL"]})
 
 from grupprum_flask import routes
--- grupprum_flask/routes.py
+++ grupprum_flask/routes.py
@@ -1,27 +1,30 @@
 from flask import request, render_template, redirect, url_for, session
-from grupprum_flask import app, db
+from grupprum_flask import app
 from datetime import datetime
 import math
 
 @app.route("/")
 @app.route("/home")
 def home():
+    # ebblog = round(db.reference("Ebb").get()/1000)
+    # now = round(datetime.now().timestamp())
+    #
+    # print("log: " + str(ebblog))
+    # print("now: " + str(now))
+    # print("difference in seconds: " + str(now - ebblog))
+    #
+    # if now - ebblog > 60:
+    #     ebbstate = True
+    # else:
+    #     ebbstate = False
 
+    return "home"
 
-    ebblog = round(db.reference("Ebb").get()/1000)
-    now = round(datetime.now().timestamp())
+@app.route("/log", methods=["POST"])
+def log():
 
-    print("log: " + str(ebblog))
-    print("now: " + str(now))
-    print("difference in seconds: " + str(now - ebblog))
-
-    if now - ebblog > 60:
-        ebbstate = True
-    else:
-        ebbstate = False
-
-    return render_template("index.html", ebbstate=ebbstate)
-
+    print(request)
+    return "POST request got"
 # @app.route("/receive", methods=["POST"])
 # def receive():
     # data = request.json()
